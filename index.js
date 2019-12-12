659c258a4b4c9d429bc70d29d7e3ab052f3fa85b
Nuttapol Phomthon <sevipsagis@gmail.com>
2019-12-12 10:40:24 +0700

    Add catch error in index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const fs = require('fs');
 const eventYear = process.argv[2];
 const eventDay = process.argv[3]
+try {
+    const input1 = fs.readFileSync(`./inputs/${eventYear}/Day${eventDay}/part1.txt`, 'utf-8');
+    const input2 = fs.readFileSync(`./inputs/${eventYear}/Day${eventDay}/part2.txt`, 'utf-8');
 
-const input1 = fs.readFileSync(`./inputs/${eventYear}/Day${eventDay}/part1.txt`, 'utf-8');
-const input2 = fs.readFileSync(`./inputs/${eventYear}/Day${eventDay}/part2.txt`, 'utf-8');
+    const solution1 = require(`./solutions/${eventYear}/Day${eventDay}/part1.js`);
+    const solution2 = require(`./solutions/${eventYear}/Day${eventDay}/part2.js`);
 
-const solution1 = require(`./solutions/${eventYear}/Day${eventDay}/part1.js`);
-const solution2 = require(`./solutions/${eventYear}/Day${eventDay}/part2.js`);
+    console.log(solution1.solve(input1));
+    console.log(solution2.solve(input2));
 
-console.log(solution1.solve(input1));
-console.log(solution2.solve(input2));
+} catch (err) {
+    console.log(err);
+}
